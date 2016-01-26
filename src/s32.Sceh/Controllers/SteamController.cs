c4049e20e7b96a997f678685e65745ddb7a974c8
Bartosz Charchut <bartosz.charchut@3soft.pl>
2016-01-26 16:15:00 +0100

    OpenId

---
+++ src/s32.Sceh/Controllers/SteamController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DotNetOpenAuth.Messaging;
+using s32.Sceh.Code.OpenId;
+using s32.Sceh.Models;
+
+namespace s32.Sceh.Controllers
+{
+    public class SteamController : Controller
+    {
+        private readonly OpenIdMembershipService openidemembership;
+
+        public SteamController()
+        {
+            openidemembership = new OpenIdMembershipService();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            var user = openidemembership.GetUser();
+            if (user != null)
+            {
+                var cookie = openidemembership.CreateFormsAuthenticationCookie(user);
+                HttpContext.Response.Cookies.Add(cookie);
+
+                return new RedirectResult(Request.Params["ReturnUrl"] ?? "/");
+            }
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(string provider)
+        {
+            var response = openidemembership.ValidateAtOpenIdProvider(provider);
+
+            if (response != null)
+            {
+                return response.RedirectingResponse.AsActionResult();
+            }
+
+            return View();
+        }
+    }
+}
