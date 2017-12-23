b29376d8846232b8df7155f9f0b8c35dd88b5a43
Sinus32 <sinus32@tlen.pl>
2017-12-23 23:39:28 +0100

    Performance improvements

--- src/s32.Sceh.WinApp/Controls/LazyImage.cs
+++ src/s32.Sceh.WinApp/Controls/LazyImage.cs
@@ -13,22 +13,51 @@ using System.Windows.Media.Imaging;
 using s32.Sceh.Code;
 using s32.Sceh.DataModel;
 using s32.Sceh.WinApp.Code;
+using System.Windows.Threading;
+using System.Diagnostics;
 
 namespace s32.Sceh.WinApp.Controls
 {
     public class LazyImage : Image
     {
+        public static readonly DependencyProperty DelayedLocalFilePathProperty =
+            DependencyProperty.Register("DelayedLocalFilePath", typeof(string), typeof(LazyImage), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty DelayProperty =
+            DependencyProperty.Register("Delay", typeof(int), typeof(LazyImage), new PropertyMetadata(0));
+
         public static readonly DependencyProperty ImageFileProperty =
             DependencyProperty.Register("ImageFile", typeof(ImageFile), typeof(LazyImage), new PropertyMetadata(null, ImageFileChanged));
 
         public static readonly DependencyProperty LocalFilePathProperty =
-            DependencyProperty.Register("LocalFilePath", typeof(string), typeof(LazyImage), new PropertyMetadata(null));
+            DependencyProperty.Register("LocalFilePath", typeof(string), typeof(LazyImage), new PropertyMetadata(null, LocalFilePathChanged));
 
         public static readonly DependencyProperty NewFilePriorityProperty =
             DependencyProperty.Register("NewFilePriority", typeof(DownloadPriority), typeof(LazyImage), new PropertyMetadata(null));
 
         public static readonly DependencyProperty OldFilePriorityProperty =
             DependencyProperty.Register("OldFilePriority", typeof(DownloadPriority), typeof(LazyImage), new PropertyMetadata(null));
+
+        private bool _delayFinished;
+        private DispatcherTimer _timer;
+
+        public LazyImage()
+        {
+            this.Loaded += LazyImage_Loaded;
+            this.Unloaded += LazyImage_Unloaded;
+        }
+
+        public int Delay
+        {
+            get { return (int)GetValue(DelayProperty); }
+            set { SetValue(DelayProperty, value); }
+        }
+
+        public string DelayedLocalFilePath
+        {
+            get { return (string)GetValue(DelayedLocalFilePathProperty); }
+            set { SetValue(DelayedLocalFilePathProperty, value); }
+        }
 
         public ImageFile ImageFile
         {
@@ -58,6 +87,51 @@ namespace s32.Sceh.WinApp.Controls
         {
             if (e.NewValue != null)
                 ImageLoadNotifier.OrderImage((ImageFile)e.NewValue, (LazyImage)d, ((LazyImage)d).NewFilePriority, ((LazyImage)d).OldFilePriority);
+        }
+
+        private static void LocalFilePathChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var self = (LazyImage)d;
+            if (self._delayFinished && self.LocalFilePath != null)
+                self.DelayedLocalFilePath = self.LocalFilePath;
+        }
+
+        private void LazyImage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (Delay > 0)
+            {
+                if (_timer == null)
+                {
+                    _timer = new DispatcherTimer(DispatcherPriority.ApplicationIdle, this.Dispatcher);
+                    _timer.Interval = TimeSpan.FromMilliseconds(Delay);
+                    _timer.Tick += Timer_Tick;
+                }
+                _timer.Start();
+            }
+            else
+            {
+                _delayFinished = true;
+                if (LocalFilePath != null)
+                    DelayedLocalFilePath = LocalFilePath;
+            }
+        }
+
+        private void LazyImage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (_timer != null)
+                _timer.Stop();
+            _delayFinished = false;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            ((DispatcherTimer)sender).Stop();
+            if (IsLoaded)
+            {
+                _delayFinished = true;
+                if (_delayFinished && LocalFilePath != null)
+                    DelayedLocalFilePath = LocalFilePath;
+            }
         }
     }
 }
